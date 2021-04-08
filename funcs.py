3d5fa24dae34fd71e3e8463331ab57f324151e80
LORD-GL <boikogv06@gmail.com>
2021-04-08 18:11:31 +0300

    bot v 0.1

--- funcs.py
+++ funcs.py
@@ -7,13 +7,18 @@ class App():
         self.bot = telebot.TeleBot(self.conf.HTTP_API)
 
     def init_keyboards(self):
-        self.main_menu_keyboard_init()
+        self.main_menu_init()
         self.help_menu_init()
+        self.end_menu_init()
 
-    def main_menu_keyboard_init(self):
+    def main_menu_init(self):
         self.main_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
         self.main_keyboard.row('Приобрести курс', 'Информация')
 
     def help_menu_init(self):
         self.connect_buy_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
         self.connect_buy_keyboard.row('Связь с админом', 'Оплата')
+
+    def end_menu_init(self):
+        self.end_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+        self.end_keyboard.row('Связь с админом', '/start')
--- handlers.py
+++ handlers.py
@@ -1,18 +1,31 @@
 # file with handlers
 def bot_start(bot, App):
     App.init_keyboards()
-    # Start menu
+    # Start
     @bot.message_handler(commands=['start'])
     def start_menu(message):
         grettings = """ Приветствие! """
         bot.send_message(message.chat.id, grettings, reply_markup = App.main_keyboard)
 
-    # Inf menu
+    # Inf
     @bot.message_handler(func = lambda message: message.text == "Информация")
-    def Help(message):
+    def help(message):
         price = """
 Обычный - 100 руб
 С куратором - 200 руб 
 VIP - 300 руб
                 """
         bot.send_message(message.chat.id, price, reply_markup = App.connect_buy_keyboard)
+
+    # pay
+    @bot.message_handler(func = lambda message: message.text == "Оплата" or message.text == "Приобрести курс")
+    def pay(message):
+        requisites = """ 
+MoноБанк   1111111111111
+ПриватБанк 1111111111111"""
+        bot.send_message(message.chat.id, requisites, reply_markup = App.end_keyboard)
+
+    # admin help
+    @bot.message_handler(func = lambda message: message.text == "Связь с админом")
+    def adm_help(message):
+        bot.send_message(message.chat.id, "Наш админ: @kidkate_moto", reply_markup = App.end_keyboard)
