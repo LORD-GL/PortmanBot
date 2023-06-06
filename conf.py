7869c3dec18dd8fc2799dfed3995283d695a6122
Hlib (Rudi) <55025513+LORD-GL@users.noreply.github.com>
2023-06-06 22:52:37 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,4 @@
 #configuration class file
 class Conf():
     def __init__(self):
-        #self.HTTP_API = "1788019605:AAEE3dR0BpzSaiaN2xZcdcqyw49SKxc6UOY" //MY
-        self.HTTP_API = "1720198317:AAHhBGMMZv6xuMLYvJzORNBEEupw5BGsflo"  #KATE
+        self.HTTP_API = ""  #KATE
